93b9af245edee1d79ebb292bbd159efb116ed174
Dafrok <88609546@qq.com>
2015-12-23 12:28:25 +0800

    build

--- src/js/game/animation.js
+++
@@ -1,8 +0,0 @@
-var Animation = require('../vendor/animation.js');
-
-//var GameAnimation = function (option) {
-//    Animation(option);
-//}
-
-//module.exports = GameAnimation;
-module.exports = Animation;
