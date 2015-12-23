d227f6187909f4d0111427f3f15856d5c2522dde
Dafrok <88609546@qq.com>
2015-12-23 16:23:24 +0800

    test

---
+++ dist/js/source.js
@@ -0,0 +1,122 @@
+(function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);throw new Error("Cannot find module '"+o+"'")}var f=n[o]={exports:{}};t[o][0].call(f.exports,function(e){var n=t[o][1][e];return s(n?n:e)},f,f.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
+/**
+ * @file game source
+ */
+
+// 测试
+var __uri = function (arg) {
+    var prefix = 'http://dafrok.github.io/game-pick-it-up/';
+    return prefix + arg;
+};
+// 测试
+
+module.exports = {
+    player: {
+        width: 153,
+        height: 165,
+        speed: 8,
+        image: {
+            left: {
+                name: 'left',
+                map: ['0 0', '0 165', '0 330', '0 495'],
+                state: [
+                    __uri('resource/image/player-left-0.png'),
+                    __uri('resource/image/player-left-1.png'),
+                    __uri('resource/image/player-left-2.png'),
+                    __uri('resource/image/player-left-3.png'),
+                    __uri('resource/image/player-left-4.png')
+                ]
+            },
+            right: {
+                name:'right',
+                map: ['0 0', '0 165', '0 330', '0 495'],
+                state: [
+                    __uri('resource/image/player-right-0.png'),
+                    __uri('resource/image/player-right-1.png'),
+                    __uri('resource/image/player-right-2.png'),
+                    __uri('resource/image/player-right-3.png'),
+                    __uri('resource/image/player-right-4.png')
+                ]
+            },
+            stopLeft: {
+                name: 'stop-left',
+                state: [
+                    __uri('resource/image/player-left-0.png'),
+                    __uri('resource/image/player-left-1.png'),
+                    __uri('resource/image/player-left-2.png'),
+                    __uri('resource/image/player-left-3.png'),
+                    __uri('resource/image/player-left-4.png')
+                ]
+            },
+            stopRight: {
+                default:true,
+                name: 'stop-right',
+                state: [
+                    __uri('resource/image/player-right-0.png'),
+                    __uri('resource/image/player-right-1.png'),
+                    __uri('resource/image/player-right-2.png'),
+                    __uri('resource/image/player-right-3.png'),
+                    __uri('resource/image/player-right-4.png')
+                ]
+            },
+            stop: {
+                name: 'stop',
+                state: []
+            }
+        }
+    },
+    items: [
+        {
+            name: '糖果',
+            score: 500,
+            image: __uri('resource/image/candy.png'),
+            weight: 1000,
+            width: 70,
+            height: 70
+        },
+        {
+            name: '小南瓜1',
+            score: 800,
+            image: __uri('resource/image/pumpkin-s1.png'),
+            weight: 800,
+            width: 54,
+            height: 54
+        },
+        {
+            name: '小南瓜2',
+            score: 800,
+            image: __uri('resource/image/pumpkin-s2.png'),
+            weight: 800,
+            width: 54,
+            height: 54
+        },
+        {
+            name: '大南瓜',
+            score: 1000,
+            image: __uri('resource/image/pumpkin-b.png'),
+            weight: 500,
+            width: 73,
+            height: 80
+        },
+        {
+            name: '幽灵',
+            score: -300,
+            image: __uri('resource/image/ghost.png'),
+            weight: 300,
+            width: 70,
+            height: 70,
+            map: ['0 70', '0 140', '0 210', '0 280', '0 350']
+        }
+    ],
+    score: {
+        color: {
+            add: 'yellow',
+            decrease: 'red'
+        },
+        size: 28,
+        duration: 2,
+        flyHeight: 150
+    }
+};
+
+},{}]},{},[1])
